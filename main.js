f98e625e599768536d69259e3884188b1c99dd28
dylandennondoesntexist <dylandennon@gmail.com>
2025-08-02 09:14:29 -0400

    5char geohash

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // --- Geohashing ---
   const BASE32 = "0123456789bcdefghjkmnpqrstuvwxyz";
-  function geohash(lat, lng, precision = 6) {
+  function geohash(lat, lng, precision = 5) {
     let latRange = [-90, 90], lngRange = [-180, 180], hash = "", bit = 0, ch = 0, isEven = true;
     while (hash.length < precision) {
       const mid = isEven ? (lngRange[0] + lngRange[1]) / 2 : (latRange[0] + latRange[1]) / 2;
@@ -453,7 +453,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
   aboutMenu.addEventListener('click', () => {
     closeMenuDropdown();
-    showModal('About', "Inspired by Ho'oponopono & The Pitt. <br><br> Press a button to display a dot on the map.               Location is converted to an approximate 6-character geohash. No personal identifiers are stored or shared.");
+    showModal('About', "Inspired by Ho'oponopono & The Pitt. <br><br> Press a button to display a dot on the map.               Location is converted to an approximate 5-character geohash. No personal identifiers are stored or shared.");
   });
 
   document.addEventListener('keydown', e => {
